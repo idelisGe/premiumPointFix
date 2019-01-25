c5ed1699ccb0e86d0497d6c2c28f22482a60c0b6
Antonio Silva <gsisa.asilva@gmail.com>
2019-01-25 11:07:39 -0600

    [IMP] sale_premiumpaint: warehouse name in header excel report valuation

--- sale_premiumpaint/wizard/export_quant_line.py
+++ sale_premiumpaint/wizard/export_quant_line.py
@@ -29,33 +29,50 @@ class QuantValuationExport(models.TransientModel):
         this = self[0]
         domain = [('product_id.type', '=', 'product'),('location_id.usage', '=', 'internal')]
         quant_data = self.env['stock.quant'].read_group(domain,fields=['product_id','location_id','quantity'], groupby=['product_id','location_id'])
+        locations = self.env['stock.location'].search([('usage', '=', 'internal')])
+        print (locations)
+        location_qty = {'product_id': '', 'quantity': 0.0, 'inventory_value': 0.0}
+        location_header = {}
+        for location in locations:
+            warehouse = self.env['stock.warehouse'].search([('lot_stock_id', '=', location.id)], limit=1)
+            location_qty.setdefault(location.id, 0.0)
+            location_header.setdefault(location.id, warehouse.name if warehouse else location.name)
         quant_result = []
         for qd in quant_data:
             product = self.env['product.product'].browse(qd['product_id'][0])
-            quant_item = {'product_id': qd['product_id'][1], 'location_id': 'Todas','quantity': qd['quantity'], 'inventory_value': product.stock_value}
-            quant_result.append(quant_item)
+            quant_item = location_qty.copy()
+            quant_item.update({'product_id': qd['product_id'][1], 'quantity': qd['quantity'], 'inventory_value': product.stock_value})
             if '__domain' in qd:
+                quant_item.update({'product_id': qd['product_id'][1], 'inventory_value': product.stock_value})
                 quant_location = self.env['stock.quant'].read_group(qd['__domain'],fields=['location_id','quantity'], groupby=['location_id'])
                 for ql in quant_location:
-                    quant_item = {'product_id': qd['product_id'][1], 'location_id': ql['location_id'][1],'quantity': ql['quantity'], 'inventory_value': ''}
-                    quant_result.append(quant_item)
-
+                    quant_item.update({ql['location_id'][0]: ql['quantity']})
+            quant_result.append(quant_item)
         with contextlib.closing(io.BytesIO()) as buf:
             workbook = xlwt.Workbook()
             worksheet = workbook.add_sheet('Reporte Valoración Inventario')
             header_bold = xlwt.easyxf("font: bold on; pattern: pattern solid, fore_colour gray25;")
-            
-            headers = self._get_headers()
-            for (col, value) in enumerate(headers):
+
+            worksheet.write(0,0,'Producto',header_bold)
+            key_locations = []
+            col = 1
+            for key,value in location_header.items():
                 worksheet.write(0,col,value,header_bold)
+                key_locations.append(key)
+                col += 1
+            worksheet.write(0,col,'Cantidad Total',header_bold)
+            worksheet.write(0,col + 1,'Valor',header_bold)
 
             row = 1
             for qr in quant_result:
                 cell_num = xlwt.easyxf(num_format_str="#,##0.00")
                 worksheet.write(row,0,qr['product_id'])
-                worksheet.write(row,1,qr['location_id'])
-                worksheet.write(row,2,qr['quantity'])
-                worksheet.write(row,3,qr['inventory_value'],cell_num)
+                col = 1
+                for key_location in key_locations:
+                    worksheet.write(row,col,qr[key_location],cell_num)
+                    col += 1
+                worksheet.write(row,col,qr['quantity'],cell_num)
+                worksheet.write(row,col + 1,qr['inventory_value'],cell_num)
                 row += 1
 
             workbook.save(buf)
